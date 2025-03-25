dac6043f1fe64f8fa5eb51283ffcb41627d4b902
NexusSensei <tudela.mathieu@gmail.com>
2025-03-25 17:54:49 +0100

    working on add profile

--- frontend/components/profile/add.js
+++ frontend/components/profile/add.js
@@ -2,30 +2,93 @@
 
 // shadcn
 import Link from "next/link"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-
-
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 
 const AddProfile = () => {
 
 
 
-    return (
-      <div className="flex justify-center items-center p-10">
-        <Card className="w-2/3">
+  return (
+    <div className="flex justify-center items-center min-h-[80vh] p-4">
+      <Tabs defaultValue="Client" className="w-[600px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="Client">Client</TabsTrigger>
+          <TabsTrigger value="Bijoutier">Bijoutier</TabsTrigger>
+        </TabsList>
+        <TabsContent value="Client">
+          <Card>
             <CardHeader>
-                <CardTitle className="flex justify-center items-center">Choisissez un rôle :</CardTitle>
+              <CardTitle>Client</CardTitle>
+              <CardDescription>
+                Consultez la marcket-place et gérez vos certificats.
+              </CardDescription>
             </CardHeader>
-            <CardContent>
-                <div className="flex justify-center gap-4">
-                    <Link href="/" className="px-4 py-2 text-blue-600 hover:text-blue-800">Bijoutier</Link>
-                    <Link href="/marcketplace" className="px-4 py-2 text-blue-600 hover:text-blue-800">Client</Link>
-                </div>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Nom & Prénom</Label>
+                <Input id="name" defaultValue="votre nom..." />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" defaultValue="votremail@golchain.com" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="Adresse">Adresse</Label>
+                <Input id="Adresse" defaultValue="Paris" />
+              </div>
             </CardContent>
-        </Card>
-      </div>
-    )
+            <CardFooter>
+              <Button>Créer profil client</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="Bijoutier">
+          <Card>
+            <CardHeader>
+              <CardTitle>Bijoutier</CardTitle>
+              <CardDescription>
+                Créez des certificats, ajoutez vos bijoux à la marcket-place.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="Name">Dénomination sociale</Label>
+                <Input id="Name" defaultValue="votre dénomination..." />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" defaultValue="votremail@golchain.com" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="Adresse">Adresse</Label>
+                <Input id="Adresse" defaultValue="Paris" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Créer profil bijoutier</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
   }
   
   export default AddProfile
